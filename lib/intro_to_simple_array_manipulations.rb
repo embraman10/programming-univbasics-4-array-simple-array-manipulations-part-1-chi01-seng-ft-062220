021b5174e089bddd50cc0ec1671f3d43b8e1fa80
Emily Braman <embraman10@gmail.com>
2020-06-13 21:50:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,7 +14,7 @@ end
 
 def pop_with_args(array)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  small_dogs = dog_breeds(2).pop
+  dog_breeds(2).pop
 end
 
 def using_shift(array)
